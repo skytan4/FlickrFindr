beab58aa227e2f63eafe89f4199f2760b47e8002
Skyler Tanner <skyler.tanner@nike.com>
2019-09-20 19:57:04 -0700

    update app delegate to programmatically set rootView

--- FlickrFindr/FlickrFindr/AppDelegate.swift
+++ FlickrFindr/FlickrFindr/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        
         return true
     }
 
