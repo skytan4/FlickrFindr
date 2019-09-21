c908c073e645a14178f927b4a3785c02c6127832
Skyler Tanner <skyler.tanner@nike.com>
2019-09-20 19:56:09 -0700

    Change NetworkController to have static methods
    - Add to url path string the values to request a thumbnail image and a larger image.
    - Add method to load an image URL.
    - Update Endpoint errors to support image loading failures as well as capturing an optional error string for generic service failures.
    - Remove no longer needed print lines

--- FlickrFindr/FlickrFindr/Controllers/NetworkController.swift
+++ FlickrFindr/FlickrFindr/Controllers/NetworkController.swift
@@ -6,25 +6,26 @@
 //  Copyright © 2019 Skyler Tanner . All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /* Example Reference
  https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=1508443e49213ff84d566777dc211f2a&text=tigers&media=photos&extras=url_q%2C+url_sq&per_page=25&page=&format=json&nojsoncallback=1
 */
 
 struct NetworkController {
-    private let apiKey = "1508443e49213ff84d566777dc211f2a"
+    static private let apiKey = "1508443e49213ff84d566777dc211f2a"
     
     enum EndpointError {
         case nilURL
         case nilData
         case decodeFailure(DecodingError)
-        case serviceError(String)
+        case serviceError(String?)
+        case imageLoadFailed(String?)
     }
     
-    func searchImages(searchTerm: String, page: Int, resultsPerPage: Int = 25, callback: @escaping (SearchResult?, EndpointError?) -> Void) {
+    static func searchImages(searchTerm: String, page: Int, resultsPerPage: Int = 25, callback: @escaping (SearchResult?, EndpointError?) -> Void) {
   
-        guard let imageSearchURL = URL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&text=\(searchTerm)&media=photos&extras=url_q%2C+url_sq&per_page=\(resultsPerPage)&page=\(page)&format=json&nojsoncallback=1") else {
+        guard let imageSearchURL = URL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(NetworkController.apiKey)&text=\(searchTerm)&media=photos&extras=url_q%2C+url_sq&per_page=\(resultsPerPage)&page=\(page)&format=json&nojsoncallback=1") else {
             callback(nil, .nilURL)
             return
         }
@@ -39,7 +40,6 @@ struct NetworkController {
             
             guard let responseData = data else {
                 callback(nil, .nilData)
-                print("Error getting data")
                 return
             }
             
@@ -47,8 +47,6 @@ struct NetworkController {
                 let result = try JSONDecoder().decode(SearchResult.self, from: responseData)
                 callback(result, nil)
             } catch (let error) {
-                
-                print(error)
                 if let err = error as? DecodingError {
                     callback(nil, .decodeFailure(err))
                 } else {
@@ -57,9 +55,23 @@ struct NetworkController {
                 }
                 return
             }
-
         }
         
         dataTask.resume()
     }
+    
+    static func loadImage(url: URL, completed callBack: (UIImage?, EndpointError?) -> Void) {
+        do {
+            let imageData = try Data(contentsOf: url)
+            let image = UIImage(data: imageData)
+            callBack(image, nil)
+        } catch (let error) {
+            callBack(nil, .imageLoadFailed(error.localizedDescription))
+        }
+    }
+    
+    static func cancelNetworkCalls() {
+        URLSession.shared.invalidateAndCancel()
+    }
+    
 }
